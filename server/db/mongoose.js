83e1c6ca8f31ed2d2f8343d8f658ff6a2de6186f
Ilay Bokobza <kanoel200@gmail.com>
2020-10-29 07:35:26 +0200

    monogo link auto switch

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
+const os = require('os')
+const keys = require('../secret/keys')
+
+let url = null
+
+
+if(os.hostname() == 'IlayBIgPc'){
+    url = 'mongodb://127.0.0.1:27017/quiz-app'
+}else{
+    url = keys.mongodbKey
+}
+
 
 //connects to db
-mongoose.connect('mongodb://127.0.0.1:27017/quiz-app',{
+mongoose.connect(url,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true,
